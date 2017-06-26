e947b7b1ab778e7e16a85633fd1b1278cd8f534b
Chris Grace <dev.chris.grace3@gmail.com>
2017-06-25 22:09:07 -0500

    Initial vampire program

---
+++ ruby/vampires.rb
@@ -0,0 +1,22 @@
+puts "What's your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "Great, so what year were you born?"
+birth_year = gets.chomp.to_i
+puts "Our cafeteria serves garlic bread. Would you like some? (y/n)"
+garlic_bread_b = gets.chomp.downcase
+puts "Would you like to enroll in our health insurance program? (y/n)"
+h_insurance = gets.chomp.downcase
+
+if name == "Tu Fang" || name == "Drake Cula"
+  puts "Definitely a vampire"
+elsif (age == (2017 - birth_year)) && (garlic_bread_b == "y" || h_insurance == "y")
+  puts "Probably not a vampire."
+elsif (age != (2017 - birth_year)) && (garlic_bread_b != "y" || h_insurance != "y")
+  puts "Probably a vampire."
+elsif age != (2017 - birth_year) && garlic_bread_b != "y" && h_insurance != "y"
+  puts "Amost certainly a vampire."
+else
+  puts "Results inconclusive."
+end
