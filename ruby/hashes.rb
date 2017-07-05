5d701cd1a8ba0dd7b488a5a2e51249be38ad710a
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-05 18:44:09 -0500

    add initial hashes pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,7 @@
+# Write a program that will allow an interior designer to enter
+# details of a given clien:
+# -- Prompt user to add:
+# -- 1. Client's name
+# -- 2. Age
+# -- 3. Number (#) of children
+# -- 4. Decor theme 
