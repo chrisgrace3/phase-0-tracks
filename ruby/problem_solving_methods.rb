e93d9618690f26a65d160a5a58640a7bddb53425
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-09 20:37:08 -0500

    add fib sequence to problem solving methods ruby file

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,4 @@
+# RELEASE 0
 # Take an array and a number, evaluate each object in the array
 # such that the index of the item prints if same as number provided in argument.
 
@@ -18,3 +19,21 @@ end
 
 arr = [42, 89, 23, 1]
 search(arr, 42)
+
+# RELEASE 1
+# Calculate Fibonacci Numbers
+# Input will be a number
+# Return an array
+
+def fib(num)
+  fib_array = []
+  init_array = [0,1]
+  cycle = 0
+  num.times do
+    init_array << init_array[-1] + init_array[-2]
+    fib_array << init_array[cycle]
+    cycle += 1
+  end
+  p fib_array
+end
+fib(100)
