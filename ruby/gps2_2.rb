e949c7bbabbaa9408884108ca3e337e4253b2ba1
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-11 15:28:26 -0500

    Further edits to gps 2.2 ruby script

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -15,37 +15,61 @@ def create_list(list_input)
   p list
 end
 
-
 # Method to add an item to a list
 # input: list, item name, and optional quantity
 # steps:
   # Create method in taking the input arguments.  Within method add parameters key with optional quality as value.
   # Assign new item to a key with a value of 1, IF no optional quantity
 # output: HASH with newly added item
-
-def list_adder(list_input_adder, item_name_add, optional_quanity = 1)
-  list_input_adder[item_name_add] = optional_quanity
-  p list_input_adder
+def item_adder(list, item, optional_quantity = 1)
+  list[item] = optional_quantity
+  p list
 end
 
 # Method to remove an item from the list
-# input: list, item name, optional quality
+# input: list (hash), EXISTING item name, optional quantity
 # steps:
-  # Create a method that takes the list parameter above and removes the key-value parameter specified.
-# output: Hash without removed item
-def list_remover(list_input_remover, item_name_remove)
-  list_input_remover.delete(item_name_remove)
-  p list_input_remover
+  # Set optional quantity to 1, and delete item from list if equal to 1
+  # Otherwise, change the quantity of the item in the list to reflect quantity to be subtracted.
+# output: Hash with item removed
+def item_remover(list, item, optional_quantity = 1)
+  if optional_quantity == 1
+    list.delete_if { |key, value|
+     key == item
+    }
+  else
+    list.each do |key, value|
+      if item == key
+        list[key] = (value - optional_quantity)
+      end
+    end
+  end
+p list
 end
+
 # Method to update the quantity of an item
-# input:
+# input: list (hash), existing item, new quantity
 # steps:
-# output:
+  #
+# output: New hash with an updated quantity of an item
+def update_quanity(list, item, new_quantity)
+  list.each do |key, value|
+     list[key] = new_quantity
+   end
+   p list
+end
 
 # Method to print a list and make it look pretty
-# input:
+# input: list (hash)
 # steps:
-# output:
+# output: Pretty list
+def pretty_up(list)
+  puts "\nHere is your grocery list: "
+  list.each do |key, value|
+    puts value.to_s + ' ' + key.to_s
+  end
+end
 
 # DRIVER CODE ************************************************************************************************************************
-list_remover(list_adder(create_list("carrots apples cereal pizza"), "Bagels"), "cereal")
+groceries = {"carrots"=>1, "apples"=>1, "cereal"=>1, "pizza"=>1, "oranges"=>5}
+pretty_up(groceries)
