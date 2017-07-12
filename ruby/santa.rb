1f8dfa7b8c25b963907052832dbb74908424fbb6
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-12 18:08:00 -0500

    add celebrate_birthday method to Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -4,7 +4,7 @@ class Santa
   def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
     @gender = gender
-    @ethnicity = enthicity
+    @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
   end
@@ -18,12 +18,34 @@ class Santa
     cookie_type
   end
 
+  def celebrate_birthday
+    @age = @age + 1
+  end
+
+  def get_mad_at
+
+  end
+
 
 end
 
 
 # DRIVER CODE ****************************************************
 
-santa = Santa.new
-santa.speak
-santa.eat_milk_and_cookies("chocolate chip")
+# # create an array called Santas
+# santas = []
+# # create an array for genders and populate with all genders
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# # create an array for ethnicities and populate with same number of ethnicities as genders
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+#
+# # create a loop that iterates as many times as there are objects in one of the arrays
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+# santas << Santa.new("male", "Italian")
+# santas << Santa.new("female", "Native American")
+# p santas
+
+santa = Santa.new("female", "black")
+p santa.celebrate_birthday
