1d8315479068e9a01484efd497de20535d58ec8d
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-12 17:22:21 -0500

    Add gender attribute to Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,17 +1,19 @@
+# Create Santa class
 class Santa
+
+  def initialize(gender)
+    puts "Initializing Santa instance..."
+    @gender = gender
+  end
+
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
 
   def eat_milk_and_cookies(cookie_type)
-    puts "That was a good #{cookie_type}!"
+    puts "That was a good #{cookie_type} cookie!"
     cookie_type
   end
-
-  def initialize
-    puts "Initializing Santa instance..."
-  end
-
 
 
 end
@@ -19,5 +21,6 @@ end
 
 # DRIVER CODE ****************************************************
 
-st_nick = Santa.new
-st_nick.speak
+santa = Santa.new
+santa.speak
+santa.eat_milk_and_cookies("chocolate chip")
