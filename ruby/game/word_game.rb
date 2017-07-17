f2bc0e0eb4911ae3649a31584f41038e225f49e9
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-17 02:23:38 -0500

    first stab at the code. mucn more work to be done.

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -1,10 +1,21 @@
 class WordGame
-  attr_reader :solution, :solution_array, :guesses
+  attr_reader :solution, :solution_array, :guess_count, :is_over
+
 
   def initialize(entry)
     @solution = entry
     @solution_array =  entry.split('')
-    @guesses = @solution_array.length
+    @guess_count = @solution.length
+    @is_over = false
+  end
+
+  def check_guess(index)
+  	@guess_count += 1
+  	if @guess_count[index] == "ball"
+  		@is_over = true
+  	else
+  		false
+  	end
   end
 
 
@@ -14,9 +25,30 @@ end
 
 # ***********DRIVER CODE*******************************************************
 
-# puts 'Welcome to the word game!'
-# puts 'Please enter a word. Any word!'
+puts 'Welcome to the word game!'
+puts 'Please enter a word, any word!'
+entry = gets.chomp
+game = WordGame.new(entry)
 
-game = WordGame.new("Hello")
+puts "Now walk away and let somebody else guess your word!"
+puts "Please guess the word:"
+guess = gets.chomp
+count = 0
 
-p game.solution
+guesses_list = []
+
+while !game.is_over
+  if guess == entry
+    puts "Wow! You got it! Good job!"
+    break
+  elsif
+    puts "Sorry, that is incorrect."
+    puts "Please try again."
+    guess = gets.chomp
+    if guesses_list.include?(guess)
+      break
+    else
+      guesses_list << guess
+    end
+  end
+end
