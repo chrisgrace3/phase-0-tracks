17c4200b7c2cdb288dab812a89484d89cd7802bc
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-17 15:36:05 -0500

    randomness to this word game

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -1,34 +1,93 @@
 class WordGame
-  attr_reader :solution, :solution_array, :guess_count, :is_over, :incomplete_solution
-  attr_accessor :guess_array, :guess
+  attr_reader :solution_array, :solution, :guess_count
+  attr_reader :incomplete_solution_string
+  attr_reader :submitted_guesses
+  attr_writer :wrong_guesses
 
-
-  def initialize(entry) # entry = "pudding"
-    @solution = entry # @solution = "pudding"
-    @solution_array = entry.split('') # ['p','u','d','d','i','n','g']
+  def initialize(entry_string)
+    @solution = entry_string.downcase
+    @solution_array = @solution.split('')
     @solution_length = @solution_array.length
-    @guess_list = []
-    @guess_count = @solution_array.length # 7
-    @incomplete_solution = Array.new()
-    @incomplete_solution =
-    p @incomplete_solution
+    @total_guesses = []
   end
 
-  def check_guess(guess_string) # "guess"
-    incomplete_array = Array.new
-    string_array = Array.new
-    guess_array = Array.new
-    @guess = guess_string
-    guess_array = @guess.split('') # guess_array = ['g','u','e','s',s']
-    guess_array.each do |char|
-      if solution_array.include?(char)
-        incomplete_array << char
-      else
-        incomplete_array << '_'
+  def guess_array(string)
+    string.split('')
+  end
+
+  def guesses
+    @guess_count = (@solution_length.to_i - @wrong_guesses.to_i)
+    @guess_count
+  end
+
+  def check_guess(guess_string)
+    guess_string = guess_string.downcase
+    if @solution == guess_string
+      puts "Wow you are a genius! Go have yourself a cookie."
+    end
+
+    while @solution != guess_string.downcase
+    @total_guesses << guess_string
+    puts "Nope, please try again."
+    guess_string = gets.chomp.downcase
+      if !@total_guesses.include?(guess_string.downcase)
+
       end
+    puts "Nope. Try again ya big dummy."
+
     end
   end
+
+
+    if @solution != guess_string.downcase
+
+    elsif @solution
+
+      break
+    '
+    @solution != guess_string.downcase
+
+
+    @total_guesses << guess_string
+    end
+  end
+
+  def check_guess(guess_string) # def check_guess("pie")
+
+    @submitted_guess_array = []
+    @submitted_guess_array = guess_string.split('')
+    @submitted_guesses << guess_string
+      @submitted_guess_array.each do |letter|
+        if @solution_array.include?(letter)
+          @incomplete_solution_array[@solution_array.index(letter)] = letter
+        end
+      end
+    end
+
+    # @solution_length.times do
+    #   @incomplete_solution_array << " - " # = ["_", "_", etc.]           # @incomplete_solution_array = ["_", "_", "_", "_", "_", "_", "_",]
+    # end
+
+  #   @incomplete_solution_array = []
+  #   @incomplete_solution_string = @incomplete_solution_array.join('')      # @incomplete_solution_string = "_ _ _ _ _ _ _"
+  #   @submitted_guesses = []
+  #   @counts = @submitted_guesses.length
+  # end
+
+
 end
+  #
+  #     if @solution_array.include?
+  #   end
+  #   guess_array.each do |char|
+  #     if solution_array.include?(char) # if [p u d d i n g] includes p
+  #       @incomplete_solution_array.map!
+  #     else
+  #       incomplete_array << '_'
+  #     end
+  #   end
+  # end
+# end
 
     # guess_array.each do |char|
     #   if !@solution_array.include?(char)
@@ -51,12 +110,14 @@ end
 
 # ***********DRIVER CODE*******************************************************
 
-# puts 'Welcome to the word game!'
-# puts 'Please enter a word, any word!'
-# # # entry = gets.chomp.to_s
-string = "pudding"
-game = WordGame.new(string)
-game.check_guess(string)
+puts 'Welcome to the word game!'
+puts 'Please enter a word, any word!'
+entry = gets.chomp.to_s
+
+game = WordGame.new("pudding")
+game.check_guess("pie")
+p game.submitted_guesses
+
 
 
 
