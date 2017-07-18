e5610f34af650017a7696ca2216b03956af13622
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-17 21:51:29 -0500

    further attempts to make this damn program work.

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -1,72 +1,67 @@
 class WordGame
-  attr_reader :answer_string         # original correct answer
-  attr_reader :answer_array
-  attr_reader :letter_board_string   # when you have letters but not the whole word
-  attr_reader :answer_length_int     # length (number) of characters in answer
-  attr_reader :solved
-  attr_writer :all_guesses_array     # list of all unique wrong guesses
+  attr_reader   :answer_string         # original correct answer
+  attr_reader   :answer_array
+  attr_reader   :letter_board_string   # when you have letters but not the whole word
+  attr_reader   :answer_length     # length (number) of characters in answer
+  attr_reader   :letter_board_array
+  attr_writer   :all_guesses_array     # list of all unique wrong guesses
+  attr_accessor :solved
 
-  def initialize(entry_string)
-    @answer_string = entry_string.downcase
+  def initialize(entry)
+    @answer_string = entry.downcase
     @answer_array = @answer_string.split('')
-    @answer_length_int = @answer_array.length
-    @all_guesses_array = [] # list of all unique wrong guesses
-    @counter = @all_guesses_array.length
-    @guess = String.new
-    @guess_count_int =  0
+    @answer_length = entry.length
+
+    @solved = nil
+
     @letter_board_array = []
-    @solved = false
-    @answer_length_int.times do
-      @letter_board_array << " _ "
+    @answer_length.times do
+    @letter_board_array << " _ "
     end # .times do end
+    @all_guesses_array = [] # list of all unique wrong guesses
   end # initialize method end
 
 
-  def check_guess(guess) # "dog"
-    @guess_string = guess.downcase # @guess_string = "dog"
-    @guess_array = @guess_string.split('')
+  def check_guess(guess)
+    x = @answer_length
+    y = 0
 
-    loop do
-      if @answer_string == @guess_string
-        @solved = true
-        break
-      elsif @all_guesses_array.include?(@guess_string)
-        puts "You have already made that guess."
-        puts "Please try again"
-      else
-        @guess_array.each do |letter|
-          if @answer_array.include?(letter)
-            @letter_board_string = @letter_board_array.join
-            puts 'Yes, there is an ' + letter + "in the word."
-            puts 'Look:'
-            answer_index = @answer_array.index(letter)
-            @letter_board_array[answer_index] = letter
-            p @letter_board_array
-            puts "Please try again"
-          elsif
-            puts "Sorry, there is no {#letter} in the word."
-            puts "Please try again."
-            @all_guesses_array << @guess_string
-            @guess_string = gets.chomp
-          end
+    while x > y
+      guess_array = guess.split('')
+        if @answer_string == guess
+           @solved = true
+           break
+         elsif @answer_string != guess
+           guess_array.each do |x|
+             if @answer_string == guess
+                @solved = true
+                break
+             elsif @answer_array.include?(x) || (!@letter_board_array.include?(guess))
+               index = @answer_array[guess_array.index(x)]
+               @letter_board_array[index.to_i] = x
+               puts "Not quite, but you got some characters correct:"
+               puts @letter_board_array.join
+               puts "Now guess again!"
+               guess = gets.chomp.downcase
+               y += 1
+             end
+           end
+         elsif @letter_board_array.include?(guess)
+           puts "You already guessed that!"
+           puts "Try again."
         end
-      end
     end
   end
 end
 
-
 #**********DRIVER CODE************************************************************************************
+
 
 puts "Hello there!"
 puts "Give me a word for someone to guess:"
 entry = gets.chomp.downcase
 
 game = WordGame.new(entry)
-p game.letter_board_string
-puts
-puts "Okay, now walk away from the computer"
-puts "and allow someone to guess your word."
 puts
 puts
 puts
@@ -76,14 +71,41 @@ puts
 puts
 puts
 puts
+puts
+puts
+puts
+puts
+puts
+puts
+puts
+puts
+puts
+puts
+puts
+puts
+puts
+puts
+puts
+puts
+puts
+puts
+puts
+puts
+puts
+puts"Okay, now walk away from the computer"
+puts"and allow someone to guess your word."
 puts
 puts "What do you think the word is??"
-guess = gets.chomp.downcase
+reply = gets.chomp.downcase
 
-game.check_guess(guess)
+game.check_guess(reply)
 
 if game.solved == true
-  puts "Great job! You must be a psychic!"
+  puts
+  puts
+  puts "YES! The word is indeed '#{entry}''."
+  puts "Nicely done."
 else
-  puts "Wow, what a total loser you are! Just kidding. You're only slightly a loser."
+  puts "Wow. No, that is not correct at all."
+  puts "What a total loser you are! Just kidding. You\'re only slightly a loser."
 end
