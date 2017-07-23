247c48f47acdc1de911089cc724f58d59da52cdc
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-23 17:02:18 -0500

    more code that does nothing

--- js/explore.js
+++ js/explore.js
@@ -1,3 +1,16 @@
-// split the letters of a word and put each in a list. 
+// split the letters of a word and put each in a list.
 // reverse the list
 // combine the letters together again
+
+
+
+var reverse = function(string) {
+for (i = 0, i < string.length, i++) {
+  string.first
+};
+  };
+
+
+// *******DRIVER CODE*********
+
+reverse("hello")
