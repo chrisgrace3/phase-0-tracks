99e56ed12afdb8e9a2220dc80a457ac933e67068
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-24 11:00:16 -0500

    modify reverse function

--- js/explore.js
+++ js/explore.js
@@ -1,16 +1,30 @@
-// split the letters of a word and put each in a list.
-// reverse the list
-// combine the letters together again
+// reverse function should work using the string index
+// take a string as an input and runs a FOR loop
+// create an empty string variable to add the letters in from the word backwards
+// in the for loop, create variable that will incrememnt backwards from length of string to 0
+// continue the loop until is is equal to 0
+// continually subtract one from the incrememnt variable and add the letter at
+// the current index as you count down.
 
 
-
-var reverse = function(string) {
-for (i = 0, i < string.length, i++) {
-  string.first
-};
-  };
-
+function reverse(word) {
+  var reverseWord = "";
+  for (var i = word.length - 1; i >= 0; i--) {
+    reverseWord += word[i];
+  }
+  return reverseWord;
+}
 
 // *******DRIVER CODE*********
 
-reverse("hello")
+console.log(reverse("hello"))
+
+// => olleh
+
+
+
+// DRIVER CODE
+var word = reverse("word") == "drow";
+console.log(word);
+var javaScript = reverse("JavaScript") == "tpircSavaJ";
+console.log(javaScript);
