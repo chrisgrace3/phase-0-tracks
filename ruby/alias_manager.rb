976c211f52cabe0b7f72538e669e2d53ad9237b9
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-27 20:11:24 -0500

    add name encrypter

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,8 +8,9 @@ Take a name (e.g. "Felicia Torres") and create a fake name:
 --Change consonants to the next consonant.
 --Print alias to user
 =end
-vowels = ['a','e','i','o','u']
-consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','r','v','w','x','y','z']
+vowels = ['a','e','i','o','u','a']
+consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','r','v','w','x','y','z','b']
+
 
 print "Enter your first and last name: "
 full_name = gets.chomp
@@ -23,21 +24,41 @@ end
 first_name = full_name.split(' ')[0]
 last_name = full_name.split(' ')[1]
 
-p first_name_array = first_name.chars
-p last_name_array = last_name.chars
+first_name_array = first_name.chars
+last_name_array = last_name.chars
 
-def encryptor(name_array)
-  encrypted_name = ""
-  name_array.each do |char|
-    if vowels.include?(char) || char != 'u'
-      encrypted_name.push(vowels)
+def next_vowel(char)
+  vowels = ['a','e','i','o','u','a']
+  original_index =  vowels.index(char)
+  next_vowel = vowels[original_index + 1]
+end
 
-    else
+def next_consonant(char)
+  consonants_array = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','b']
+  original_index =  consonants_array.index(char)
+  next_consonant = consonants_array[original_index + 1]
+end
 
-
-    end
+new_first_name = ""
+new_last_name = ""
+first_name_array.each do |char|
+  if vowels.include?(char)
+    new_first_name << next_vowel(char)
+  else
+    new_first_name << next_consonant(char)
   end
 end
 
+last_name_array.each do |char|
+  if vowels.include?(char)
+    new_last_name << next_vowel(char)
+  else
+    new_last_name << next_consonant(char)
+  end
+end
+
+codename = new_last_name.capitalize + ' ' + new_first_name.capitalize
+
+p codename
 
 # Driver code
