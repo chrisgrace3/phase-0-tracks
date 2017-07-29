2e3c909509a9a3c2a115398062b75677b689d9ed
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-28 22:25:23 -0500

    add kitten class with initialize and two additional methods. cread 50 instances of kitten class and perform each method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,10 @@
 class Puppy
 
+  def initialize
+    puts "Initializing new puppy instance ... "
+
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -25,17 +30,37 @@ class Puppy
 
 end
 
+class Kitten
+
+  def initialize
+    puts "Initializing kitten class ..."
+  end
+
+  def pet
+    puts "purrr..."
+  end
+
+  def give_catnip
+    puts "*goes nuts*"
+  end
+
+end
+
 # ****************** DRIVER CODE ************************************************************************
 
-
 fido = Puppy.new
-
 fido.fetch("ball")
-
 fido.speak(2)
-
 fido.roll_over
-
 p fido.dog_years(3)
-
 fido.play_dead
+
+playpen = []
+50.times do
+  playpen << Kitten.new
+end
+
+playpen.each do |kitten|
+  kitten.give_catnip
+  kitten.pet  
+end
