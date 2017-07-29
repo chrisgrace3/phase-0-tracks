91483ca6ac42d05dd1fcb750b7b29f5ee140483e
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-28 22:10:44 -0500

    add Puppy methods per 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,4 +5,37 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    int.times do
+      puts "Woof!"
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(int)
+    int * 7
+  end
+
+  def play_dead
+    puts "*plays dead*"
+  end
+
 end
+
+# ****************** DRIVER CODE ************************************************************************
+
+
+fido = Puppy.new
+
+fido.fetch("ball")
+
+fido.speak(2)
+
+fido.roll_over
+
+p fido.dog_years(3)
+
+fido.play_dead
