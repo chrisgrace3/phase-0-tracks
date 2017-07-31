fd30d5a9eb418bc65226e51fbf224f4cf1ff021a
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-31 14:39:49 -0500

    add get mad at and celebrate birthday instance methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -8,7 +8,17 @@ class Santa
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen",
       "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
+  end
 
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer)
+    #remove from array
+    @reindeer_ranking.delete(reindeer)
+    # add to end of array
+    @reindeer_ranking.push(reindeer)
   end
 
   def speak
@@ -31,7 +41,13 @@ example_genders = ["agender", "female", "bigender", "male", "female", "gender fl
 example_ethnicities = ["black", "Latino", "white", "Japanese-African",
   "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 
-santas = []
-example_genders.length.times do
-  santas << Santa.new(example_genders[rand(0..6)], example_ethnicities[rand(0..6)])
-end
+# santas = []
+# example_genders.length.times do
+#   santas << Santa.new(example_genders[rand(0..6)], example_ethnicities[rand(0..6)])
+# end
+
+santa = Santa.new("agender", "black")
+p santa
+santa.get_mad_at("Prancer")
+santa.celebrate_birthday
+p santa
