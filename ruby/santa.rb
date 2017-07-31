5653028feec8af5ff7f628311d856c4e4ad12d33
Chris Grace <dev.chris.grace3@gmail.com>
2017-07-31 14:10:23 -0500

    add diverse initializations as driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,5 @@
 class Santa
+  attr_reader
 
   def initialize(gender, ethnicity)
     puts "Initializing Santa instance ..."
@@ -7,6 +8,7 @@ class Santa
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen",
       "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
+
   end
 
   def speak
@@ -25,8 +27,11 @@ end
 
 # ***** DRIVER CODE ************************************************************
 
-nick = Santa.new
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African",
+  "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 
-nick.speak
-
-nick.eat_milk_and_cookies('sugar cookie')
+santas = []
+example_genders.length.times do
+  santas << Santa.new(example_genders[rand(0..6)], example_ethnicities[rand(0..6)])
+end
