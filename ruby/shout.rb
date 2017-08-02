b1762dd8d1578a1108efdcf4cb43baff6930142b
Chris Grace <dev.chris.grace3@gmail.com>
2017-08-02 15:47:09 -0500

    add driver code to test module methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -4,6 +4,10 @@ module Shout
   end
 
   def self.yell_happily(words)
-    words + "!!!" + " :)"    
+    words + "!!!" + " :)"
   end
 end
+
+puts Shout.yell_angrily("Get off my lawn")
+
+puts Shout.yell_happily("I'm king of the world")
