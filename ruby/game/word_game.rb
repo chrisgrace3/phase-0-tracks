baef497b4a12d683f91fcf721485c3c17c821f1a
Chris Grace <dev.chris.grace3@gmail.com>
2017-08-04 18:59:11 -0500

    updates to game stuff

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -1,4 +1,4 @@
-# step 1
+  # step 1
 # input: word (string)
 # steps:
 #   1. split up the word by letter and put into a hash with "_" for each value
@@ -6,26 +6,49 @@
 # output: string as underscores and message asking for word to be guessed
 
 class WordGame
-  attr_accessor :word_checker, :guess_counter
+  attr_accessor :word_checker, :guess_counter, :win
 
-  def initialize
-    @word = ""
+  def initialize(word)
+    @word = word.upcase
     @guesses = 0
-    @guess_counter = 0
+    @guess_counter = word.length
     @is_over = false
-    @word_checker = []
+    @word_checker = @word.split('')
+    @fill_out = Array.new(word.length, "_")
+    @win = false
+
   end
 
-  def input_word(word)
-    @word = word
-    @guess_counter = word.length
-    @word_checker = word.split('')
+  def check_guess(guess)
+    if guess == @word
+      @win = true
+      @is_over = true
+    else
+      if (guess.length >
+
+      end
+
+    end
+
+
   end
 
 end
 
 # ***** DRIVER CODE *******************************************************
-game = WordGame.new
-game.input_word("hello")
-p game.word_checker
-p game.guess_counter
+game = WordGame.new("hello")
+
+while !game.is_over
+  puts "Guess the word! You may enter a letter or a whole word if you think you know it."
+  guess = gets.chomp.downcase
+  if !game.check_guess
+
+
+  end
+end
+
+if game.win
+  puts "Congrats!"
+else
+  puts "Sorry, you lose. Goodbye."
+end
