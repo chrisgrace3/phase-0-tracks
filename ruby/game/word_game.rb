ceb14404d6e0e6018a5d18ce428a7b3083fbf0d6
Chris Grace <dev.chris.grace3@gmail.com>
2017-08-04 23:05:29 -0500

    add guess letter method and test

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -1,2 +1,46 @@
 class WordGame
+  attr_reader :answer, :game_over, :chances, :letter_board_reveal
+  attr_accessor :guess
+
+  def initialize(word)
+    @answer = word.upcase
+    @game_over = false
+    @chances = word.length
+    @guess = ''
+    @letter_board_reveal = "_" * word.length
+  end
+
+  def guess_letter(letter)
+    @guess = letter.upcase
+    if @letter_board_reveal.include?(@guess)
+      puts "You already guessed that letter."
+    else
+      if @answer.include?(@guess)
+        
+        @letter_board_reveal[@answer.index(@guess)] = @guess
+
+        true
+      else
+        false
+      end
+    end
+  end
+end
+
+# User Interface
+
+puts "Hello! And welcome to the worst word game ever made."
+puts "Please enter a word for someone to guess: "
+game = WordGame.new(gets.chomp)
+  puts "Please guess a letter."
+
+while !game.game_over
+  puts "You have #{game.chances} chances remaining."
+  letter = gets.chomp
+   if game.guess_letter(letter)
+     puts "You got one!"
+     puts game.letter_board_reveal
+   else
+     puts "Sorry, that letter is not in the word."
+   end
 end
--- ruby/game/word_game_spec.rb
+++ ruby/game/word_game_spec.rb
@@ -1,11 +1,19 @@
 require_relative 'word_game'
 
 describe WordGame do
-    let(:game) { WordGame.new("river") }
+    let(:game) { WordGame.new("word") }
 
 
   it "stores and returns given solution" do
-    expect(game.answer).to eq ("river")
+    expect(game.answer).to eq ("WORD")
+  end
+
+  it "returned the length of the word as guesses allowed" do
+    expect(game.chances).to eq 4
+  end
+
+  it "check to see if incorrect, and this should be correct" do
+    expect(game.check_guess('i')).to eq ("_i___")
   end
 
   it "check to see if correct, and this should be correct" do
