98fbea24c0bea95ba23980cb9efe409353bd7cda
Chris Grace <dev.chris.grace3@gmail.com>
2017-08-08 12:08:49 -0500

    implement reverse method

--- js/explore.js
+++ js/explore.js
@@ -1,9 +1,9 @@
 // reverse function should work using the string index
-// take a string as an input and runs a FOR loop
+// take a string as an input and run a FOR loop
 // create an empty string variable to add the letters in from the word backwards
-// in the for loop, create variable that will incrememnt backwards from length of string to 0
+// in the for loop, create variable that will increment backwards from length of string to 0
 // continue the loop until is is equal to 0
-// continually subtract one from the incrememnt variable and add the letter at
+// continually subtract one from the increment variable and add the letter at
 // the current index as you count down.
 
 
@@ -15,8 +15,6 @@ function reverse(word) {
   return reverseWord;
 }
 
-// *******DRIVER CODE*********
+var word = reverse("WORLD")
 
-console.log(reverse("hello"))
-
-// => olleh
+console.log(word)
