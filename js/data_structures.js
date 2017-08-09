f5846e8ece8e02281db44eed6f7064848e6d9711
Chris Grace <dev.chris.grace3@gmail.com>
2017-08-09 16:02:02 -0500

    push new object to colors and horses array. add driver code to test

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,2 +1,8 @@
 var colors = ["blue", "green", "red", "indigo"];
 var horses = ["Duke", "Lucky", "Ed", "Spirit"];
+
+colors.push("orange");
+horses.push("Chief");
+
+console.log(colors);
+console.log(horses);
