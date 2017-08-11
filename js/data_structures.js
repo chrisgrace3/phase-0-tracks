b6a03a1fef18d1e49cf56670909ad50491b9e6a7
Chris Grace <dev.chris.grace3@gmail.com>
2017-08-11 12:18:17 -0500

    add car constructor function and create two cars

--- js/data_structures.js
+++ js/data_structures.js
@@ -12,3 +12,16 @@ for (var i = 0; i < colors.length; i++) {
 	horseStable[horses[i]] = colors[i]
 }
 console.log(horseStable)
+
+function Car(year, make, model, isNew) {
+  this.year = year;
+	this.make = make;
+	this.model = model;
+	this.isNew = isNew;
+	this.honk = function () { console.log("Beep beep!");};
+}
+
+var car = new Car(1989, "Chevy", "Celebrity", false)
+var anotherCar = new Car(2006, "Ford", "Taurus", false)
+console.log(anotherCar.make)
+console.log("Unfortunately my car is a", car.year, car.make, car.model)
