c2a94bf3b6fc2bd33c10c9bce984cddadd144378
Chris Grace <dev.chris.grace3@gmail.com>
2017-08-13 12:29:18 -0500

    refactor longest phrase function

--- js/algos.js
+++ js/algos.js
@@ -1,13 +1,21 @@
-function longestPhrase(phrases) {
-  var newPhrase = "";
+function findLongestPhrase(phrases) {
+  var longestPhrase = phrases[0];
   for (var i = 0; i < phrases.length; i++) {
-    if (phrases[i].length > newPhrase.length ) {
-      newPhrase = phrases[i];
+    if (phrases[i].length > longestPhrase.length ) {
+      longestPhrase = phrases[i];
     }
   }
-  return newPhrase;
+  return longestPhrase;
 }
 
-// Driver code to test longestPhrase function
-var collectionOfPhrases = ["long phrase", "longest phrase","longer phrase"];
-console.log(longestPhrase(collectionOfPhrases))
+// ******DRIVER CODE***********
+
+// Release 1: Find a Key-Value Match
+// console.log(sharePair({name: "Steven", age: 54}, {name: "Tamir", age: 54})); // true
+// sharePair({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}); //  true
+
+
+// Release 0: Find the longest phrase
+console.log(findLongestPhrase(["big", "bigger", "biggestest", "even more bigger than biggestest"]));
+console.log(findLongestPhrase(["longest phrase in the world!", "nope", "not this one"]));
+console.log(findLongestPhrase(["long phrase","longest phrase","longer phrase"]));
